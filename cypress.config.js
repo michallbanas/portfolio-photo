fedea6294aff7d3f589f54831f34560c2d6c8055
michalbanas <michal.banas@kiwi.com>
2022-06-10 17:22:18 +0200

    change: photo improvements

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   component: {
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     devServer: {
       baseUrl: "http://localhost:3000",
       framework: "create-react-app",
-      bundler: "webpack",
+      bundler: "webpack"
     },
   },
 });
--- src/data/images-data.jsx
+++ src/data/images-data.jsx
@@ -1,21 +1,57 @@
 const data = [
     {   
-        imageSrc: "/images/fruit.jpg",
-        name:"Plum on the tree",
-        altText: "black and white picture of plum on the tree",
+        imageSrc: "/images/autumnMountains.jpg",
+        name:"Autumn Mountains",
+        altText: "Black and white picture of autumn mountains",
         key: "01",
     },
     {
-        imageSrc: "/images/mountain.jpg",
-        name:"Mountain",
-        altText: "mountain",
+        imageSrc: "/images/brokenTree.jpg",
+        name:"Broken Tree",
+        altText: "Black and white picture of a broken tree in the meadow",
         key: "02",
     },
     {
-        imageSrc: "/images/country.jpg",
-        name: "Country",
-        altText: "country",
+        imageSrc: "/images/fruit.jpg",
+        name: "Plums",
+        altText: "Black and white picture of plums on the tree",
         key: "03",
+    }, 
+    {
+        imageSrc: "/images/mountain.jpg",
+        name: "Mountain",
+        altText: "Black and white picture of a mountains - West Tatras",
+        key: "04",
+    }, 
+    {
+        imageSrc: "/images/country.jpg",
+        name: "Countryside",
+        altText: "Black and white picture of a countryside",
+        key: "05", 
+    },
+    {
+        imageSrc: "/images/shadowTree.jpg",
+        name: "Shadow Tree",
+        altText: "Black and white picture of a tree with a shadow",
+        key: "06",
+    },
+    {
+        imageSrc: "/images/oldHouse.jpg",
+        name: "Old House",
+        altText: "Black and white picture of an old house",
+        key: "07",
+    },
+    {
+        imageSrc: "/images/girlInTheForest.jpg",
+        name: "Girl in the Forest",
+        altText: "Black and white picture of an old house",
+        key: "08"
+    },
+    {
+        imageSrc: "/images/animals.jpg",
+        name: "Animals",
+        altText: "Black and white picture of the animals",
+        key: "09"
     }
 ]
 
